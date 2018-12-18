70c4525191acdb8968498d1aae7103235fb9d09f
grandmike <853006033@qq.com>
2018-12-17 23:28:23 -0600

    add contacts

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
 # Create your views here.
 
 def register(req):
@@ -61,5 +62,11 @@ def logout(req):
     else:
         return redirect('index')
 
-def dashboard(req):
-    return render(req, 'accounts/dashboard.html')
+def dashboard(request):
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+    context = {
+    'contacts': user_contacts
+    }
+    
+    return render(request, 'accounts/dashboard.html', context)
--- real_estate/settings.py
+++ real_estate/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -142,3 +143,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 
 }
+
+#email
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER=''
+EMAIL_HOST_PASSWORD=''
+EMAIL_USE_TLS=True
